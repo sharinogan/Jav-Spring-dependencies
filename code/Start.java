c593ae58dcc371d5a67b4b4523f7261c98d96448
Voragun Supawong <voragun2543@gmail.com>
2022-09-16 16:50:55 +0700

    list all beans

--- code/Start.java
+++ code/Start.java
@@ -8,13 +8,15 @@ class Start {
     public static void main(String[] args) {
         GenericApplicationContext context;
         context = new GenericApplicationContext();
-        context.registerBean(Shop.class);
+        context.registerBean("first", Shop.class);
+        context.registerBean("second", Shop.class);
+        context.registerBean("third", Shop.class);
         context.refresh();
         
-        Shop s = context.getBean(Shop.class);
-        s.name = "iCoffee";
-        s.contact = "Mr.Bill, 0946573928";       
-        System.out.println(s.name);
+//        Shop s = context.getBean(Shop.class);
+//        s.name = "iCoffee";
+//        s.contact = "Mr.Bill, 0946573928";       
+//        System.out.println(s.name);
         
         String[] all = context.getBeanDefinitionNames();
         for(int i = 0; i < all.length; i++) {

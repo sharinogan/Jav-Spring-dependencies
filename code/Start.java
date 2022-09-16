4fa409ce7440820dcfdb1dbfde205eeae313a42b
Voragun Supawong <voragun2543@gmail.com>
2022-09-16 17:11:24 +0700

    [GenericApplicationContext]

--- code/Start.java
+++ code/Start.java
@@ -8,20 +8,35 @@ class Start {
     public static void main(String[] args) {
         GenericApplicationContext context;
         context = new GenericApplicationContext();
+        
+        // Assume that we are at the building. it has 3 floor.
         context.registerBean("first", Shop.class);
         context.registerBean("second", Shop.class);
         context.registerBean("third", Shop.class);
         context.refresh();
         
-//        Shop s = context.getBean(Shop.class);
-//        s.name = "iCoffee";
-//        s.contact = "Mr.Bill, 0946573928";       
-//        System.out.println(s.name);
+        Shop s;
+        s = context.getBean("first", Shop.class);
+        s.name = "Whitecat dessert";
+        
+        s = context.getBean("second", Shop.class);
+        s.name = "Hamburger Quickly";
+        
+        s = context.getBean("third", Shop.class);
+        s.name = "iMovie Studio";
         
         String[] all = context.getBeanDefinitionNames();
         for(int i = 0; i < all.length; i++) {
             System.out.println(all[i]);
         }
+        
+        // version 1
+        s = context.getBean("first", Shop.class);
+        System.out.println(s.name); // Whitecat dessert
+        
+        // version 2
+        s = (Shop)context.getBean("second");
+        System.out.println(s.name); // Hamburger Quickly
     }
 }
 

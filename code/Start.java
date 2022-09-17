64386c6828f1a204687d714dff07a863dfb6bea4
Voragun Supawong <voragun2543@gmail.com>
2022-09-17 10:11:35 +0700

    [ClassPathXmlApplicationContext]

--- code/Start.java
+++ code/Start.java
@@ -1,46 +1,36 @@
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.core.SpringVersion;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 class Start {
     public static void main(String[] args) {
-        GenericApplicationContext context;
-        context = new GenericApplicationContext();
+        ApplicationContext context;
+        context = new ClassPathXmlApplicationContext("start.xml");
         
-        // Assume that we are at the building. it has 3 floor.
-        context.registerBean("first", Shop.class);
-        context.registerBean("second", Shop.class);
-        context.registerBean("third", Shop.class);
-        context.refresh();
+        Shop s = context.getBean("first", Shop.class);
+        System.out.println(s.name);
         
-        Shop s;
-        s = context.getBean("first", Shop.class);
-        s.name = "Whitecat dessert";
-        
-        s = context.getBean("second", Shop.class);
-        s.name = "Hamburger Quickly";
-        
-        s = context.getBean("third", Shop.class);
-        s.name = "iMovie Studio";
+        s = (Shop)context.getBean("third");
+        System.out.println(s.name);
         
         String[] all = context.getBeanDefinitionNames();
         for(int i = 0; i < all.length; i++) {
             System.out.println(all[i]);
         }
-        
-        // version 1
-        s = context.getBean("first", Shop.class);
-        System.out.println(s.name); // Whitecat dessert
-        
-        // version 2
-        s = (Shop)context.getBean("second");
-        System.out.println(s.name); // Hamburger Quickly
     }
 }
 
 class Shop {
-    String name;
+    public String name;
     String contact;
+    
+    public void setName(String s) {
+        name = s;
+    }
+    
+    public void setContact (String x) {
+        contact = x;
+    }
 }
+
+class Whatever { }
